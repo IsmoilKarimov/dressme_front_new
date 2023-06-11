46e3f51d61411773824b787bec0bf098180c2fd4
main <ismoilergashovich@gmail.com>
2023-06-11 12:22:39 +0500

    icon changes

--- src/AssetsMain/icons.js
+++ src/AssetsMain/icons.js
@@ -1307,7 +1307,7 @@ const BasketIcons = ({ colors }) => {
     <svg
       width="20"
       height="20"
-      viewBox="0 0 20 20"
+      viewBox="0 1 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
@@ -1350,7 +1350,7 @@ const ActiveBasketIcons = ({ colors }) => {
     <svg
       width="20"
       height="20"
-      viewBox="0 0 20 20"
+      viewBox="0 1 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
