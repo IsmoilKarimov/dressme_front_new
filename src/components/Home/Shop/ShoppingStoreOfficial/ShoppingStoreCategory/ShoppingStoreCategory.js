254df30ae253c3ffbd9988a400e375d1b9bce43b
main <ismoilergashovich@gmail.com>
2023-06-20 13:11:20 +0500

    new changes

--- src/components/Home/Shop/ShoppingStoreOfficial/ShoppingStoreCategory/ShoppingStoreCategory.js
+++ src/components/Home/Shop/ShoppingStoreOfficial/ShoppingStoreCategory/ShoppingStoreCategory.js
@@ -9,7 +9,6 @@ const ShoppingStoreCategory = () => {
   return (
     <div className="max-w-[1280px] w-[100%] ss:px-4 md:px-0 flex justify-center items-center m-auto">
       <div className="w-[100%] h-fit">
-        {" "}
         <div className="w-full  relative flex flex-gap-6 justify-between md:my-10 my-3">
           <div className="hidden md:block md:w-[22%] h-full ">
             <ShopOfficialBrand />
