a4078bf242070e2a96644bbb7d7c0ab28072e6c5
main <ismoilergashovich@gmail.com>
2023-06-20 16:12:59 +0500

    add

--- src/components/Category/CategoryForType/CategoryElement/CategoryCards.js
+++ src/components/Category/CategoryForType/CategoryElement/CategoryCards.js
@@ -141,7 +141,7 @@ export default function CategoryCards() {
 
   return (
     <div className="flex flex-col box-border mt-2 md:mt-[34px]">
-      <div className=" w-full md:hidden">
+      {/* <div className="w-full hidden">
         <ClothesFilterGroup _class="items gap-x-2 ">
           {wearGroup?.map((data) => {
             return (
@@ -156,7 +156,7 @@ export default function CategoryCards() {
             );
           })}
         </ClothesFilterGroup>{" "}
-      </div>
+      </div> */}
       <div className="w-full h-full ">
         <div className="hidden md:block">
           <span className="not-italic font-AeonikProMedium text-sm  leading-4 text-black tracking-[1%]">
