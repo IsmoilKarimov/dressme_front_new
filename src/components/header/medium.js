68443f478caed3c8ca9241491d2634f8c3c4f0bc
main <ismoilergashovich@gmail.com>
2023-07-04 16:33:21 +0500

    added some changes

--- src/components/header/medium.js
+++ src/components/header/medium.js
@@ -430,10 +430,9 @@ const MediumHeader = () => {
           {/* Ending of Full Screen page sesction Map*/}
 
           {/* Opening of Main Menu at the Mobile version */}
-          <div
-            className={`max-w-[440px] w-[100%] fixed bg-red-500 top-[70px] left-0 right-0 bottom-0 h-screen	pb-[140px] px-3 ${
+          <div className={`max-w-[440px] w-[100%] z-50 fixed bg-red-500 top-[70px] left-0 right-0 bottom-0 h-screen	pb-[140px] px-3 ${
               state?.hamburgerMenu
-                ? " flex flex-col ease-linear bg-green-500 duration-500 overscroll-none z-[110]"
+                ? " flex flex-col ease-linear bg-green-500 duration-500 overscroll-none"
                 : "left-[-800px] z-[-80] ease-linear duration-500"
             }`}
           >
