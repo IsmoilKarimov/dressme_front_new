b10fdc72c35aad3f6bfa9139409a6ba2e578b50a
main <ismoilergashovich@gmail.com>
2023-07-17 15:28:24 +0500

    new changes in map

--- src/components/YandexMap/YandexMapsDressMe.js
+++ src/components/YandexMap/YandexMapsDressMe.js
@@ -159,8 +159,7 @@ function YandexMapsDressMe() {
       <div className="w-[100%] h-[100vh] border-b border-searchBgColor overflow-hidden">
         {/* Laptop device for */}
         {screenSize.width > 768 && (
-          <div
-            className={`w-full bottom-[0px]  overflow-hidden  md:w-[769px] fixed md:left-1/2 md:right-1/2 md:translate-x-[-50%] md:translate-y-[-50%]
+          <div className={`w-full bottom-[0px]  overflow-hidden  md:w-[769px] fixed md:left-1/2 md:right-1/2 md:translate-x-[-50%] md:translate-y-[-50%]
           ${
             dressInfo?.yandexOpenMarketLocation
               ? `z-[102] h-[350px]  bottom-[-75px]`
@@ -171,8 +170,7 @@ function YandexMapsDressMe() {
           </div>
         )}
         {screenSize.width <= 768 && (
-          <div
-            className={`fixed w-full bg-white z-[102] left-0 right-0 overflow-hidden  ${
+          <div className={`fixed w-full bg-white z-[102] left-0 right-0 overflow-hidden  ${
               dressInfo?.yandexOpenMarketLocation
                 ? "h-[562px] bottom-0 ease-linear duration-300 "
                 : "h-0 bottom-0 ease-linear duration-300 "
@@ -183,8 +181,7 @@ function YandexMapsDressMe() {
         )}
 
         {/* Navbaryandex */}
-        <div
-          className={`absolute z-50 hidden md:block ${
+        <div className={`absolute z-50 hidden md:block ${
             !dressInfo?.yandexOpenMenu
               ? "top-0 ease-linear duration-500 "
               : "top-[-250px] ease-linear duration-500 "
@@ -192,8 +189,7 @@ function YandexMapsDressMe() {
         >
           <YandexMapsIndex />
         </div>
-        <div
-          className={`absolute z-50 right-2 ${
+        <div className={`absolute z-50 right-2 ${
             dressInfo?.yandexOpenMenu
               ? "top-2  right-2 ease-linear duration-500 "
               : "top-[-250px]  right-2 ease-linear duration-500 "
@@ -211,24 +207,9 @@ function YandexMapsDressMe() {
             height="100%"
             modules={["control.FullscreenControl"]}
           >
-            {/* Yandex Shopping Card */}
-            {!dressInfo?.yandexOpenMarket && (
-              <div
-                onClick={handleOpenMarket}
-                className={`absolute right-2 md:hidden cursor-pointer ${
-                  !dressInfo?.yandexFullScreen
-                    ? "bottom-[180px]"
-                    : "bottom-[115px]"
-                }   w-[40px]  z-[52]  h-[40px] rounded-lg bg-white flex items-center justify-center`}
-              >
-                <div className="w-full h-full flex items-center justify-center">
-                  <MarketIcons colors={"#000"} />
-                </div>
-              </div>
-            )}
             <div
               onClick={handleFullScreen}
-              className={`absolute right-2 ${
+              className={`absolute right-3 ${
                 !dressInfo?.yandexFullScreen
                   ? "bottom-[128px] md:bottom-[87px]"
                   : "bottom-[65px] md:bottom-[87px]"
@@ -277,262 +258,12 @@ function YandexMapsDressMe() {
                     iconImageSize: [32, 32],
                   }}
                   modules={["geoObject.addon.balloon"]}
-                  properties={
-                    {
-                      // balloonContentHeader: `<div class="balloonContentHeader"><a class="title" href = "#">Пункт выдачи</a><br><span class="description11">${data?.address}</span></div>`,
-                      // // Зададим содержимое основной части балуна.
-                      // balloonContentBody: <YandexLocationMarketOpen />,
-                      // balloonContentBody:
-                      //   `<div class="bodyImgs"><img  className="data" src="https://images.wbstatic.net/PickupOffice/Img154040_Photo1.jpg"/><img  className="data" src="https://images.wbstatic.net/PickupOffice/Img154040_Photo1.jpg"/> </div><br/>` +
-                      //   `<div class="bodySana">
-                      //       <span class='text'>Режим работы:<span>${data?.workTime}</span></span><br/>
-                      //       <span class='text'>Примерочные: <span>${data?.imgs.length} шт</span></span>
-                      //   </div><br/>` +
-                      //   `<div class="BtnUzGroup"><div class='BtnUz'>Выбрать</div></div>`,
-                      // balloonContentFooter: `<div class="footerText"><span>Directions:</span> ${data?.direction}</div>`,
-                    }
-                  }
                 />
               ))}
             </Clusterer>
-            {/* yandex menu Open Full Code */}
-            <div className="relative">
-              {!dressInfo?.yandexOpenMenu ? (
-                <div
-                  className={`  ease-linear duration-300 h-[40px] absolute cursor-pointer ss:top-[-200px] md:top-[8px] left-[8px] z-50 bg-white shadow-lg overflow-hidden rounded-lg  `}
-                >
-                  <div
-                    onClick={handleOpenMenu}
-                    className="w-fit flex items-center justify-between  cursor-pointer roundedn-lg h-full  "
-                  >
-                    <div className="group w-10 hover:w-[138px] bg-bgCard hover:bg-white   ease-linear duration-300 rounded-lg overflow-hidden  flex items-center justify-between ">
-                      <span className="w-[36px] h-8 flex items-center justify-center">
-                        <span className="ml-[2px]">
-                          <MenuOpenIcons />
-                        </span>
-                      </span>
-                      <span className=" flex flex-nowrap items-center mr-[-100px] group-hover:mr-[10px] w-[92px] ease-linear duration-300 pt-[2px] justify-center text-center  cursor-pointer bg-white not-italic font-AeonikProMedium text-sm text-black tracking-[1%] ">
-                        Магазины
-                      </span>
-                    </div>
-                  </div>{" "}
-                </div>
-              ) : null}
 
-              <div
-                className={`${
-                  dressInfo?.yandexOpenMenu || dressInfo?.yandexOpenMarket
-                    ? " ml-[0px]"
-                    : "  ml-[-1000px]"
-                } absolute cursor-pointer left-0 h-[100vh] z-[102] rounded-lg overflow-hidden ${
-                  dressInfo?.yandexFullScreen
-                    ? "top-[0px]"
-                    : "ss:top-[70px] md:top-0"
-                } ${
-                  !dressInfo?.yandexOpenMarket
-                    ? "w-[25%] bg-yandexNavbar backdrop-blur-sm	"
-                    : "bg-white w-[100%]  "
-                }  p-2 ease-linear duration-[600ms]   `}
-              >
-                {!dressInfo?.yandexOpenMarket ? (
-                  <div
-                    onClick={handleOpenMenu}
-                    className="w-full h-[42px] flex items-center justify-center"
-                  >
-                    {" "}
-                    <div className="absolute left-2 top-2 w-[40px] h-[40px] rounded-lg bg-white  border border-searchBgColor flex items-center justify-center">
-                      <span>
-                        <MenuCloseIcons />
-                      </span>
-                    </div>
-                    <div className="w-fit ">
-                      <span className="not-italic font-AeonikProMedium text-xl leading-6 text-center tracking-[1%] text-black">
-                        Магазины
-                      </span>
-                    </div>
-                  </div>
-                ) : null}
-                {/* yandex Menu Top */}
-                <div className="w-full h-12 flex items-center justify-between px-3 rounded-lg bg-white mt-3 border border-searchBgColor">
-                  <div className="w-fit pr-3">
-                    <span className="w-6 h-6">
-                      <MarketIcons colors={"#000"} />
-                    </span>
-                  </div>
-                  <div className="w-[82%] h-full  ">
-                    <input
-                      className="w-[100%] h-full "
-                      type="text"
-                      name="search"
-                      placeholder="Поиск магазина (имя или район)"
-                    />
-                  </div>
-                  <div className="w-fit border-l pl-3 border-searchBgColor">
-                    <button type="button">
-                      <SearchIcons />
-                    </button>
-                  </div>
-                </div>
-                {/* Yandex Menu List */}
-                <div className="w-full h-[85vh] mt-3 py-1 flex flex-col gap-y-2 ">
-                  <div className="w-full h-full overflow-y-auto  YandexListScroll ">
-                    {dressInfo?.MarketList?.map((data) => {
-                      return (
-                        <div
-                          key={data?.id}
-                          className={`w-full relative ${
-                            data?.accordion ? "h-[426px]" : "h-[202px]"
-                          } border border-searchBgColor flex flex-col duration-300 rounded-lg   bg-white  overflow-hidden   mt-3`}
-                        >
-                          <div
-                            className={`w-full p-4 !h-[202px] ${
-                              data?.accordion ? "bg-white" : "bg-btnBgColor"
-                            } absolute top-0 flex flex-wrap content-around `}
-                          >
-                            <div className="w-full flex justify-between">
-                              <span className="not-italic font-AeonikProMedium text-lg leading-5 text-black tracking-[1%]">
-                                Button (Чиланзар)
-                              </span>
-                              <span className="flex">
-                                <span>
-                                  <StarIcons />
-                                </span>
-                                <span className="not-italic ml-[6px] flex font-AeonikProMedium text-lg leading-5 text-black tracking-[1%]">
-                                  4.8
-                                </span>
-                              </span>
-                            </div>
-                            <div className="w-full flex">
-                              <span>
-                                <LocationIcons />
-                              </span>
-                              <span className="w-[70%] not-italic ml-4 font-AeonikProRegular text-base leading-5 text-setTexOpacity">
-                                г. Ташкент, Чиланзарский район, квартал-7, д 45б
-                                (Катартал)
-                              </span>
-                            </div>
-                            <div className="w-full flex items-center">
-                              <span>
-                                <ClothesHangIcons colors={"#000"} />
-                              </span>
-                              <span className="not-italic ml-4 font-AeonikProRegular text-base leading-4 text-black tracking-[1%]">
-                                Есть примерочная
-                              </span>
-                            </div>
-                            <div
-                              className="w-full flex items-center justify-between"
-                              onClick={() => handleGetId(data.id)}
-                            >
-                              <div className="flex items-center ">
-                                <span>
-                                  <ClockIcons colors={"#000"} />
-                                </span>
-                                <span className="not-italic ml-4 font-AeonikProRegular text-base leading-4 text-black tracking-[1%]">
-                                  10:00 - 20:00, без выходных
-                                </span>
-                              </div>{" "}
-                              <div className="flex items-center justify-end">
-                                <span
-                                  className={`${
-                                    data?.accordion
-                                      ? "rotate-[-180deg]"
-                                      : "rotate-0"
-                                  } ease-linear duration-300`}
-                                >
-                                  <GrFormDown size={20} />
-                                </span>
-                              </div>
-                            </div>
-                          </div>
-
-                          <div
-                            className={`h-[214px] absolute top-[202px]   overflow-hidden flex flex-wrap  content-between justify-center transition ease-in-out duration-300  w-full px-4 `}
-                          >
-                            <div className="w-full ">
-                              <div className="w-[80%] h-[2px] bg-OpacitySignIn mx-auto  mb-5 "></div>
-                              <div className="flex flex-col gap-y-3 ">
-                                <div className=" flex justify-between items-center">
-                                  <div className="flex items-center">
-                                    <span>
-                                      <ClothesIcons />
-                                    </span>
-                                    <span className="ml-4 not-italic font-AeonikProMedium text-base leading-4 text-black">
-                                      Футболки
-                                    </span>
-                                  </div>
-                                  <div className="flex justify-end items-center">
-                                    <span className="not-italic font-AeonikProMedium text-base leading-5 text-right text-setTexOpacity">
-                                      от
-                                      <span className="not-italic font-AeonikProMedium text-xl leading-5 text-right text-black">
-                                        {" "}
-                                        59 000{" "}
-                                      </span>
-                                      сум
-                                    </span>
-                                  </div>
-                                </div>
-                                <div className=" flex justify-between items-center">
-                                  <div className="flex items-center">
-                                    <span>
-                                      <ClothesIcons />
-                                    </span>
-                                    <span className="ml-4 not-italic font-AeonikProMedium text-base leading-4 text-black">
-                                      Футболки
-                                    </span>
-                                  </div>
-                                  <div className="flex justify-end items-center">
-                                    <span className="not-italic font-AeonikProMedium text-xl leading-5 text-right text-setTexOpacity">
-                                      от
-                                      <span className="not-italic font-AeonikProMedium text-xl leading-5 text-right text-black">
-                                        {" "}
-                                        59 000{" "}
-                                      </span>
-                                      сум
-                                    </span>
-                                  </div>
-                                </div>
-                                <div className=" flex justify-between items-center">
-                                  <div className="flex items-center">
-                                    <span>
-                                      <ClothesIcons />
-                                    </span>
-                                    <span className="ml-4 not-italic font-AeonikProMedium text-base leading-4 text-black">
-                                      Футболки
-                                    </span>
-                                  </div>
-                                  <div className="flex justify-end items-center">
-                                    <span className="not-italic font-AeonikProMedium text-xl leading-5 text-right text-setTexOpacity">
-                                      от
-                                      <span className="not-italic font-AeonikProMedium text-xl leading-5 text-right text-black">
-                                        {" "}
-                                        59 000{" "}
-                                      </span>
-                                      сум
-                                    </span>
-                                  </div>
-                                </div>{" "}
-                              </div>
-                              <div className="w-[80%] h-[2px] bg-OpacitySignIn mx-auto  mt-5"></div>
-                            </div>
-                            <div className="w-full h-12  flex justify-center active:scale-95	active:opacity-70  items-center rounded-lg bg-searchBgColor border border-OpacitySignIn">
-                              <span>
-                                <MarketIcons colors={"#000"} />
-                              </span>
-                              <span className="ml-3 not-italic font-AeonikProMedium text-base leading-4 text-right text-black">
-                                Посетит страницу магазина
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </div>
-            </div>
             {/* Yandex Main menu */}
-            <div
-              className={`max-w-[440px] w-[100%] fixed bg-white top-[70px] left-0 h-[100vh] px-3 ${
+            <div className={`max-w-[440px] w-[100%] fixed bg-white top-[70px] left-0 h-[100vh] px-3 ${
                 dressInfo?.openMainMenu
                   ? "left-[-500px] md:left-[-5000px] z-[-80] ease-linear duration-500"
                   : "hamburger flex flex-col ease-linear duration-500 overscroll-none z-[105]"
@@ -685,8 +416,7 @@ function YandexMapsDressMe() {
               </div>
             </div>
             {/* Yandex Search */}
-            <div
-              className={`absolute  ${
+            <div className={`absolute  ${
                 !dressInfo?.yandexFullScreen ? "top-[80px]" : "top-[8px]"
               }  md:top-auto md:bottom-[24px] left-0 right-0 mx-auto  overflow-hidden z-50 bg-yandexNavbar backdrop-blur-sm rounded-xl h-[48px] w-[94%] md:w-fit shadow-lg`}
             >
@@ -699,13 +429,11 @@ function YandexMapsDressMe() {
                 }}
                 className="w-full h-full flex justify-between "
               >
-                <div className="w-[100%] h-full flex gap-x-2 items-center px-3">
+                <div className="w-full h-full flex items-center px-3">
                   <div>
-                    <span>
-                      <img src={locationIcons} alt="" />
-                    </span>{" "}
+                    <img src={locationIcons} alt="" />
                   </div>
-                  <div className="h-full flex items-center w-[240px] mx-3">
+                  <div className="h-full flex items-center w-full mx-3">
                     <input
                       type="text"
                       name="search"
