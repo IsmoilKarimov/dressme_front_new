8ac758639b27168524023afb7e7071fdcb7131c7
main <ismoilergashovich@gmail.com>
2023-09-05 11:19:24 +0500

    added new changes

--- src/components/Home/Products/SignleMainProducts/SingleProduct/ProductComment/ProductComment.js
+++ src/components/Home/Products/SignleMainProducts/SingleProduct/ProductComment/ProductComment.js
@@ -119,17 +119,12 @@ export default function ProductComment() {
           <section className="w-full py-6 flex justify-center items-center">
             <button
               onClick={() => setOpenBox(!openBox)}
-              className={`flex cursor-pointer active:scale-95 rounded-xl px-[30px] py-[10px] border border-searchBgColor active:opacity-70 items-center gap-x-3 `}
+              className={`flex cursor-pointer active:scale-95 rounded-xl px-[30px] py-[10px] border border-searchBgColor bg-bgColor active:opacity-70 items-center gap-x-3 `}
             >
               <p
-                className={`${dressInfo?.BtnSeason} bg-transparent font-AeonikProRegular text-2xl`}
+                className={`${dressInfo?.BtnSeason} bg-transparent font-AeonikProRegular text-base`}
               >
-                Bсе комментарии
-              </p>
-              <p
-                className={`${openBox ? "rotate-[0deg]" : "rotate-[180deg]"} `}
-              >
-                <ArrowTopIcons colors={dressInfo?.ColorSeason} />
+                Показать еще...
               </p>
             </button>
           </section>
