3a1598b2ae808490ce81ee903e118404fcfe3f79
main <ismoilergashovich@gmail.com>
2023-10-06 14:48:49 +0500

    new changes

--- src/components/header/top.js
+++ src/components/header/top.js
@@ -10,7 +10,7 @@ import RegionList from "../../ContextHook/RegionsList";
 const TopHeader = () => {
   const [dressInfo] = useContext(dressMainData);
   const [selectBtn, setSelectBtn] = useState(true)
-  const [regionShow, setRegionsShow] = useState(false)
+  const [regionsShow, setRegionsShow] = useState(false)
 
   const [state, setState] = useState({
     regionId: "",
@@ -150,97 +150,10 @@ const TopHeader = () => {
         <section className="max-w-[1280px] w-[100%] h-full py-[2px] flex justify-between items-center m-auto  ">
           {/* LEFT SIDE */}
           <article className="left h-full flex items-center overscroll-none overflow-y-hidden overscroll-y-none">
-            {/* <div className={`max-w-[600px] h-fit fixed px-3 md:px-6  py-2 md:py-4 bg-white rounded-b-none md:rounded-b-lg	 rounded-t-lg  mx-auto w-full duration-500 z-[113] md:top-[50%] md:left-1/2 md:right-1/2 md:translate-x-[-50%] md:translate-y-[-50%] overflow-hidden ${state?.openModalRegions ? " bottom-0 md:flex flex-col" : "md:hidden bottom-[-1500px] z-[-10] overscroll-none"}`} >
-                <div className="w-full flex items-center justify-between  ">
-                  <span className="text-black text-xl md:text-2xl not-italic font-AeonikProRegular">Выберите регион</span>
-                  <span
-                    className="select-none cursor-pointer"
-                    onClick={() => {
-                      setState({ ...state, openModalRegions: false });
-                    }}
-                  >
-                    <MenuCloseIcons colors="#000" />
-                  </span>
-                </div>
-
-                <div className="w-full overflow-auto  flex flex-col gap-y-4 pt-3  overflow-x-hidden mt-3 h-[50vh] md:h-[60vh] VerticelScroll pr-2 ">
-                  {state?.getRegionList?.regions ?
-                    state?.getRegionList?.regions?.map((data) => {
-                      // console.log(data.id);
-                      // console.log(data.name_ru);------------------------------------------------------------------
-                      return (
-                        <div key={data?.id} className={`${data.id || data.sub_regions.id ? '' : ''} w-full h-fit`}>
-                          <div className="flex items-center">
-                            <input
-                                id={data?.id}
-                                type="radio"
-                                name="region"
-                                value={data?.id}
-                                className="w-[18px] h-[18px] cursor-pointer mr-3"
-                                onClick={() => accordionCityList(data?.id)}
-                              />
-                            <label
-                              htmlFor={data?.id}
-                              className="w-full cursor-pointer flex items-center pr-1 border-b border-[#F0F0F0] "
-                            >
-                              <span
-                               className="text-[#303030] text-lg not-italic font-AeonikProRegular">
-                                {data?.name_ru}
-                              </span>
-                              <span
-                                className={`${activeIndex === data?.id ? "rotate-[-0deg] duration-300" : "rotate-[-180deg] duration-300"} ml-auto`}
-                              >
-                                <ArrowTopIcons colors={"#a1a1a1"} />
-                              </span>
-                            </label>
-                          </div>
-                          <div
-                            className={`w-full grid grid-cols-2 xs:grid-cols-3 duration-[400ms]
-                              ${activeIndex == data?.id ? "openAccardion" : "CloseAccardion"} `}
-                          >
-                            {data?.sub_regions?.map((item) => {
-                              return (
-                                <div key={item?.id} className="flex items-center px-[2px] gap-x-[4px] cursor-pointer">
-                                  <label
-                                    htmlFor={item?.name_ru}
-                                    className="flex items-center gap-x-[6px]"
-                                  >
-                                    <input
-                                      type="radio"
-                                      id={item?.name_ru}
-                                      name="region"
-                                      value={item?.region_id}
-                                      // checked={state?.subRegionId == item?.id}
-                                      className="w-4 h-4 border border-borderColor  cursor-pointer  flex items-center justify-center"
-                                      onChange={(e) => {
-                                        setState({ ...state, regionId: e.target.value, subRegionId: item?.id })
-                                      }}
-                                    />
-                                    <span className="text-[#303030]  cursor-pointer text-[15px] not-italic font-AeonikProRegular"
-                                    >{item?.name_ru}</span>
-                                  </label>
-                                </div>
-
-                              );
-                            })}
-                          </div>
-                        </div>
-                      );
-                    }) : (
-                      <p className="w-full h-full flex flex-col items-center justify-center">Ma'lumotlar yuklanayapti...</p>
-                    )}
-
-                </div>
-                <div className="w-full flex items-center justify-end  mt-2">
-                  <span onClick={() => {
-                    setState({ ...state, openModalRegions: false });
-                  }} className="cursor-pointer text-borderWinter text-lg not-italic font-AeonikProMedium">Готово</span>
-                </div>
-            </div> */}
 
             <section>
               <Link 
-                onClick={()=> setRegionsShow(!regionShow)}
+                onClick={()=> setRegionsShow(!regionsShow)}
                 to="/" className="flex w-fit items-center">
                 <LocationIcons />
                 <div className="text-textColor text-[13px] ml-2 mr-[6px] font-AeonikProMedium">
@@ -256,9 +169,9 @@ const TopHeader = () => {
                   </span>
                 </div>
               </Link>
-              <div className={`${regionShow ? '' : 'hidden'}`}>
-                <RegionList />
-                </div>
+              <div className={`${regionsShow ? '' : 'hidden'}`}>
+                <RegionList  regionsShow={regionsShow}/>
+              </div>
             </section>
 
             <section className="w-fit h-fit py-[4px] rounded bg-white font-AeonikProMedium select-none cursor-pointer">
@@ -287,8 +200,6 @@ const TopHeader = () => {
               )}
             </section>
           </article>
-
-
 
           {/* RIGHT SIDE */}
           <article className="right h-full flex items-center">
