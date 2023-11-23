534b3e0e8a2caa7eb194d345e623bfed7c48975d
main <ismoilergashovich@gmail.com>
2023-11-23 16:38:12 +0500

    data

--- src/components/Authentication/SignUp/SignUp.js
+++ src/components/Authentication/SignUp/SignUp.js
@@ -1,11 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import InputMask from "react-input-mask";
 import { dressMainData } from "../../../ContextHook/ContextMenu";
 import {
-  ArrowTopIcons,
   PersonIcons,
   PhoneIcons,
   SircleNext,
@@ -19,23 +17,53 @@ export default function SignUp() {
     firstName: "",
     lastName: "",
     phoneCode: "+998",
+    phoneNumber: "",
     email: "",
     password: "",
+    password_confirmation: "",
     eyesShow: true,
     eyesShowConfirmation: true,
     validateConfirm: true,
     requestPerson: true,
   });
 
-  let data = phone.split("-");
-  let arr = data.join("");
-  let data1 = arr.split("(");
-  let arr1 = data1.join("");
-  let arr2 = arr1.split(")");
-  let data2 = arr2.join("");
-  let arr3 = state.phoneCode.split("+");
-  let data3 = arr3.join("");
-  const sendMessagePhoneNumber = data3 + data2;
+  const url = "https://api.dressme.uz/api/user/register";
+
+  const sendRegisterData = () => {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("DressmeUserToken")}`,
+      },
+      body: JSON.stringify({
+        name: state.firstName,
+        surname: state.lastName,
+        phone: state.phoneNumber,
+        email: state.email,
+        password: state.password,
+        password_confirmation: state.password_confirmation,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res, "DATA");
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  };
+
+  // let data = phone.split("-");
+  // // let arr = data.join("");
+  // let data1 = arr.split("(");
+  // let arr1 = data1.join("");
+  // let arr2 = arr1.split(")");
+  // let data2 = arr2.join("");
+  // let arr3 = state.phoneCode.split("+");
+  // let data3 = arr3.join("");
+  // const sendMessagePhoneNumber = data3 + data2;
 
   const [dressInfo] = useContext(dressMainData);
 
@@ -62,7 +90,9 @@ export default function SignUp() {
             <div className="mt-2 w-full h-fit">
               <div className="flex items-center font-AeonikProRegular text-sm leading-4 text-black  tracking-[0,16px] ">
                 Имя{" "}
-                <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                <span className="text-red-600 ml-[2px]">
+                  <Star6Icon />
+                </span>{" "}
               </div>
               <div className="mt-[6px] px-[16px] w-full flex items-center border border-searchBgColor rounded-lg ">
                 <input
@@ -80,7 +110,9 @@ export default function SignUp() {
             <div className="mt-4 w-full h-fit">
               <div className="flex items-center font-AeonikProRegular text-sm leading-4 text-black  tracking-[0,16px] ">
                 Фамилия{" "}
-                <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                <span className="text-red-600 ml-[2px]">
+                  <Star6Icon />
+                </span>{" "}
               </div>
               <div className="mt-[6px] px-[16px] w-full flex items-center border border-searchBgColor rounded-lg ">
                 <input
@@ -98,7 +130,9 @@ export default function SignUp() {
             <div className="mt-4 w-full h-fit">
               <div className="flex items-center font-AeonikProRegular text-sm leading-4 text-black  tracking-[0,16px] ">
                 Номер телефона{" "}
-                <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                <span className="text-red-600 ml-[2px]">
+                  <Star6Icon />
+                </span>{" "}
               </div>
               <div className="flex items-center justify-center overflow-hidden border border-searchBgColor rounded-lg">
                 <div className="ss:w-[35%] md:w-[30%] h-12 flex items-center justify-center  cursor-pointer border-r border-searchBgColor overflow-hidden">
@@ -129,11 +163,11 @@ export default function SignUp() {
             {/* Email Registration Section */}
             <div className="mt-4 w-full h-fit">
               <div className=" flex items-center justify-between w-full">
-                <div
-                  className="flex items-center font-AeonikProRegular text-sm leading-4 cursor-pointer text-black  tracking-[0,16px]"
-                >
+                <div className="flex items-center font-AeonikProRegular text-sm leading-4 cursor-pointer text-black  tracking-[0,16px]">
                   Электронная почта
-                  <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                  <span className="text-red-600 ml-[2px]">
+                    <Star6Icon />
+                  </span>{" "}
                 </div>
               </div>
               <div className="mt-[6px] px-[16px] w-full flex items-center border border-searchBgColor rounded-lg ">
@@ -152,13 +186,15 @@ export default function SignUp() {
             <div className="mt-4 w-full h-fit">
               <div className="flex items-center font-AeonikProRegular text-sm leading-4 text-black  tracking-[0,16px] ">
                 Пароль
-                <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                <span className="text-red-600 ml-[2px]">
+                  <Star6Icon />
+                </span>{" "}
               </div>
               <div className="mt-[6px] px-[16px] w-full flex items-center border border-searchBgColor rounded-lg ">
                 <input
                   className="w-full h-12 placeholder-not-italic placeholder-font-AeonikProMedium placeholder-text-base placeholder-leading-4 placeholder-text-black"
                   type={state?.eyesShow ? "password" : "text"}
-                  placeholder="Enter your password"
+                  placeholder="Введите пароль"
                   required
                 />
                 <span className="cursor-pointer">
@@ -175,7 +211,7 @@ export default function SignUp() {
                   )}
                 </span>
               </div>
-              <div className="not-italic mt-2 font-AeonikProRegular selec-none text-xs leading-3 text-black tracking-[0,16px] ">
+              <div className="not-italic mt-2 font-AeonikProRegular selec-none text-xs leading-3 text-[#9CA3AF] tracking-[0,16px]">
                 Пароль должен быть не менее 8 символов
               </div>
             </div>
@@ -183,24 +219,30 @@ export default function SignUp() {
             <div className="mt-4 w-full h-fit">
               <div className="flex items-center font-AeonikProRegular text-sm leading-4 text-black  tracking-[0,16px] ">
                 Повторите пароль
-                <span className="text-red-600 ml-[2px]"><Star6Icon/></span>{" "}
+                <span className="text-red-600 ml-[2px]">
+                  <Star6Icon />
+                </span>{" "}
               </div>
               <div className="mt-[6px] px-[16px] w-full flex items-center border border-searchBgColor rounded-lg ">
                 <input
                   className="w-full h-12 placeholder-font-AeonikProMedium placeholder-text-base placeholder-leading-4 placeholder-text-black"
                   type={state?.eyesShowConfirmation ? "password" : "text"}
-                  placeholder="Enter your password"
+                  placeholder="Введите пароль"
                   required
                 />
                 <span className="cursor-pointer">
                   {state?.eyesShowConfirmation ? (
                     <AiOutlineEyeInvisible
-                      onClick={() => setState({ ...state, eyesShowConfirmation: false })}
+                      onClick={() =>
+                        setState({ ...state, eyesShowConfirmation: false })
+                      }
                       size={20}
                     />
                   ) : (
                     <AiOutlineEye
-                      onClick={() => setState({ ...state, eyesShowConfirmation: true })}
+                      onClick={() =>
+                        setState({ ...state, eyesShowConfirmation: true })
+                      }
                       size={20}
                     />
                   )}
@@ -208,7 +250,10 @@ export default function SignUp() {
               </div>
             </div>
             <NavLink
-              onClick={() => setState({ ...state, validateConfirm: false })}
+              onClick={() => {
+                sendRegisterData();
+                setState({ ...state, validateConfirm: false });
+              }}
               className="mt-8 border  cursor-pointer flex items-center justify-center border-searchBgColor w-full h-12 bg-SignInBgColor select-none rounded-lg active:scale-95	active:opacity-70	"
             >
               <span className="not-italic font-AeonikProMedium mr-2 text-base leading-4 text-center text-white tracking-[0,16px]">
