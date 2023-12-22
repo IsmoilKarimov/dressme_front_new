2e5dbd040a3d146d3c2bceae64f870d35e32736a
Sardor0795 <english.programmer.9595@gmail.com>
2023-12-22 18:18:13 +0500

    Add breadCrumbs to sections & remove mock data from pr. show page

--- src/components/Category/CategoryForType/CategoryTop/CategoryTopDetail.js
+++ src/components/Category/CategoryForType/CategoryTop/CategoryTopDetail.js
@@ -5,6 +5,7 @@ import {
   ClothesIcons,
   FilterIcons,
   GoBackIcon,
+  ItailIcons,
   SearchIcons,
   UnderSection,
 } from "../../../../assets/icons";
@@ -169,13 +170,71 @@ const CategoryTopDetail = () => {
 
   return (
     <main className="flex flex-col justify-center border-t border-searchBgColor items-center">
-      <div className="pt-8 pb-14 w-full max-w-[1280px]">
-        <NavLink
-          to="/"
-          className={`w-fit flex items-center cursor-pointer justify-start md:justify-center md:border border-borderColor2 rounded-lg mr-20 md:mr-5`}
-        >
-          <GoBackIcon />
-        </NavLink>
+      <div className="pt-8 pb-14 flex flex-col md:min-h-[44px] w-full justify-center items-center m-0 py-3 box-border border-b border-searchBgColor">
+        <section className="max-w-[1280px] h-full w-[100%] flex items-center justify-between m-auto">
+          <nav className="w-[100%] md:w-fit flex items-center p-1">
+            <ul className="h-10 w-[100%] md:w-fit flex items-center overflow-auto HorizantalScroll">
+              <li className="not-italic font-AeonikProRegular flex items-center flex-nowrap text-sm leading-4 text-black tracking-[1%] mr-[10px]">
+                <NavLink
+                  to="/"
+                  className="flex items-center whitespace-nowrap cursor-pointer pt-[4px] pr-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-black tracking-[1%]"
+                >
+                  Главная
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex  	 items-center  text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex 	whitespace-nowrap  items-center cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-black tracking-[1%]">
+                  Мужская одежда
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex items-center  text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex flex-row whitespace-nowrap items-center cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-black tracking-[1%]">
+                  Все категории
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex items-center   text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex items-center whitespace-nowrap cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-black tracking-[1%]">
+                  Спортивное
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex items-center   text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex items-center whitespace-nowrap  cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 tracking-[1%]">
+                  Кроссовки
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex items-center   text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex items-center whitespace-nowrap cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-black tracking-[1%]">
+                  Nike
+                </NavLink>
+                <span>
+                  <ItailIcons colors={"#A1A1A1"} />
+                </span>
+              </li>
+              <li className="not-italic font-AeonikProRegular flex items-center   text-sm leading-4 text-black tracking-[1%]">
+                <NavLink className="flex items-center whitespace-nowrap  cursor-pointer mt-[6px] px-[10px] not-italic font-AeonikProMedium text-sm leading-4 text-setTexOpacity tracking-[1%]">
+                  Nike RUN Sneakers (Sport Wears)
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <nav className="hidden md:flex"></nav>
+        </section>
       </div>
 
       <div className="tableSizes">
