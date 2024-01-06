392b7b6d8d7a9174ccf65efe091ed19a28de43d3
main <ismoilergashovich@gmail.com>
2024-01-06 16:28:39 +0500

    rating new changes

--- src/components/Category/CategoryForType/CategoryForBrand/FiilterForApi/CustomerReviewsFilter.js
+++ src/components/Category/CategoryForType/CategoryForBrand/FiilterForApi/CustomerReviewsFilter.js
@@ -5,33 +5,21 @@ import { BsCheckLg } from "react-icons/bs";
 function CustomerReviewsFilter({ state, setState, filter, setFilterData }) {
   const [selected, setSelected] = useState(0);
   const [clickChange, setChangeClick] = useState(false);
-  const [ratings, setRatings] = useState([
-    {
-      id: 1,
-      action: false,
-      starNumber: 1,
-    },
-    {
-      id: 2,
-      action: false,
-      starNumber: 3,
-    },
-    {
-      id: 3,
-      action: false,
-      starNumber: 3,
-    },
-    {
-      id: 4,
-      action: false,
-      starNumber: 4,
-    },
-    {
-      id: 5,
-      action: false,
-      starNumber: 5,
-    },
-  ]);
+
+  // const ratings = filter?.ratings
+  // console.log(filter?.ratings,'filter');
+
+  const [ratings, setRatings] = useState({
+    "1.0": 15,
+    "2.0": 17,
+    "3.0": 17,
+    "4.0": 22,
+    "5.0": 29,
+  });
+
+  console.log(filter);
+  console.log(ratings);
+  console.log(filter?.ratings);
 
   return (
     <div className="w-full flex flex-col items-center">
@@ -67,125 +55,89 @@ function CustomerReviewsFilter({ state, setState, filter, setFilterData }) {
           className={`flex flex-col   gap-y-3 overflow-hidden ${
             state?.customerRreviews
               ? "duration-300 h-0"
-              : "duration-300 h-[160px] mt-5"
+              : "duration-300 h-[200px] mt-5"
           } duration-300`}
         >
           {/* Field */}
-          {ratings?.map((rating) => {
-            console.log(rating);
+          {Object.keys(ratings).map((rating, index) => {
+            console.log(rating, index, "rating");
             return (
               <div
+                key={index}
                 className="flex items-center cursor-pointer select-none"
                 onClick={() => {
-                  setSelected(rating?.id);
+                  setSelected(index);
                   setChangeClick(true);
                 }}
               >
                 <div
                   className={`w-[22px] h-[22px] flex items-center  mr-[10px] rounded border border-borderColorCard`}
                 >
-                  {selected === rating?.id && (
+                  {selected === index && (
                     <span className="bg-blue-600 h-full w-full text-white flex items-center justify-center">
                       <BsCheckLg size={12} />
                     </span>
                   )}
                 </div>
                 <div className="flex items-center not-italic mr-2 font-AeonikProRegular  text-lg leading-4 text-black">
-                  <StarIcons />
-                  <StarIcons />
-                  <StarIcons />
-                  <StarIcons />
-                  <StarIcons />
+                  {(() => {
+                    if (index === 0) {
+                      return (
+                        <div className="flex items-center">
+                          <StarIcons />
+                        </div>
+                      );
+                    } else if (index === 1) {
+                      return (
+                        <div className="flex items-center">
+                          <StarIcons />
+                          <StarIcons />
+                        </div>
+                      );
+                    } else if (index === 2)
+                    return (
+                      <div className="flex items-center">
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                      </div>
+                    );
+                    else if (index === 3)
+                    return (
+                      <div className="flex items-center">
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                      </div>
+                    );
+                    else
+                    return (
+                      <div className="flex items-center">
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                        <StarIcons />
+                      </div>
+                    );
+                  })()}
                 </div>
+                <span className="text-base leading-3 font-AeonikProRegular">({filter?.ratings[rating]})</span>
               </div>
             );
           })}
-
-          {/* <div
-          className="flex items-center cursor-pointer select-none"
-          onClick={() => {setSelected(4); setChangeClick(true)}}
+        <button
+          type="button"
+          onClick={() => setSelected(0)}
+          className={`${
+            clickChange ? "flex" : "hidden"
+          } w-full flex-start text-sm text-borderWinter font-AeonikProRegular`}
         >
-          <div
-            className={`w-[22px] h-[22px] flex items-center  mr-[10px] rounded border border-borderColorCard`}
-          >
-            {selected === 4 && (
-              <span className="bg-blue-600 h-full w-full text-white flex items-center justify-center">
-                <BsCheckLg size={12} />
-              </span>
-            )}
-          </div>
-          <div className="flex items-center not-italic mr-2 font-AeonikProRegular  text-lg leading-4 text-black">
-            <StarIcons />
-            <StarIcons />
-            <StarIcons />
-            <StarIcons />
-          </div>
-        </div>
-        <div
-          className="flex items-center cursor-pointer select-none"
-          onClick={() => {setSelected(3); setChangeClick(true)}}
-        >
-          <div
-            className={`w-[22px] h-[22px] flex items-center  mr-[10px] rounded border border-borderColorCard`}
-          >
-            {selected === 3 && (
-              <span className="bg-blue-600 h-full w-full text-white flex items-center justify-center">
-                <BsCheckLg size={12} />
-              </span>
-            )}
-          </div>
-          <div className="flex items-center not-italic mr-2 font-AeonikProRegular  text-lg leading-4 text-black">
-            <StarIcons />
-            <StarIcons />
-            <StarIcons />
-          </div>
-        </div>
-        <div
-          className="flex items-center cursor-pointer select-none"
-          onClick={() => {setSelected(2); setChangeClick(true)}}
-        >
-          <div
-            className={`w-[22px] h-[22px] flex items-center  mr-[10px] rounded border border-borderColorCard`}
-          >
-            {selected === 2 && (
-              <span className="bg-blue-600 h-full w-full text-white flex items-center justify-center">
-                <BsCheckLg size={12} />
-              </span>
-            )}
-          </div>
-          <div className="flex items-center not-italic mr-2 font-AeonikProRegular  text-lg leading-4 text-black">
-            <StarIcons />
-            <StarIcons />
-          </div>
-        </div>
-        <div
-          className="flex items-center cursor-pointer select-none"
-          onClick={() => setSelected(1)}
-        >
-          <div
-            className={`w-[22px] h-[22px] flex items-center  mr-[10px] rounded border border-borderColorCard`}
-          >
-            {selected === 1 && (
-              <span className="bg-blue-600 h-full w-full text-white flex items-center justify-center">
-                <BsCheckLg size={12} />
-              </span>
-            )}
-          </div>
-          <div className="flex items-center not-italic mr-2 font-AeonikProRegular  text-lg leading-4 text-black">
-            <StarIcons />
-          </div>
-        </div> */}
+          Сбросить
+        </button>
         </article>
       </section>
-      <button
-        type="button"
-        onClick={() => setSelected(0)}
-        className={`${
-          clickChange ? "flex" : "hidden"
-        } w-full flex-start text-sm text-borderWinter font-AeonikProRegular mt-3`}
-      >
-        Сбросить
-      </button>
     </div>
   );
 }
