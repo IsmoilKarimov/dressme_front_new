08c354b4ccd93d2b46ccde5937575aceb96b6587
Sardor0795 <english.programmer.9595@gmail.com>
2024-01-30 15:28:36 +0500

    Fix wifi_loader gif, add gender&birth_date to register

--- src/components/Loading/LoadingNetwork.js
+++ src/components/Loading/LoadingNetwork.js
@@ -1,20 +1,21 @@
 import React from "react";
 import styles from "./forLoading.module.css";
 import PuffLoader from "react-spinners/PuffLoader";
-import wifiLoading from '../../assets/backTop/WiFi_Loader.gif.mp4'
+import WiFiLoader from "../../assets/backTop/wifi_loader.gif";
 export default function LoadingNetwork() {
-    return (
-        <div className="w-[100vh] h-[100vh] z-[50] flex items-center justify-center">
-            <div className="flex items-center justify-center flex-col ">
-                <div style={{
-                    backgroundImage: `url('${wifiLoading})`,
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center center'
-                }}>
-                </div>
-                <div>noProduct</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-[100vh] h-[70vh] z-[50] flex items-center justify-center">
+      <div className="w-full flex items-center justify-center">
+        <div
+          style={{
+            backgroundImage: `url('${WiFiLoader}')`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+          }}
+          className="w-[100px] h-[100px]"
+        ></div>
+      </div>
+    </div>
+  );
 }
