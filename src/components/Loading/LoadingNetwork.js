ea83b22167c0b3a742282bed94713cc8468f6737
Sardor0795 <english.programmer.9595@gmail.com>
2024-01-31 15:09:44 +0500

    Fix loader and sign up texts

--- src/components/Loading/LoadingNetwork.js
+++ src/components/Loading/LoadingNetwork.js
@@ -4,17 +4,19 @@ import PuffLoader from "react-spinners/PuffLoader";
 import WiFiLoader from "../../assets/backTop/wifi_loader.gif";
 export default function LoadingNetwork() {
   return (
-    <div className="w-[100vh] h-[70vh] z-[50] flex items-center justify-center">
-      <div className="w-full flex items-center justify-center">
-        <div
-          style={{
-            backgroundImage: `url('${WiFiLoader}')`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center center",
-          }}
-          className="w-[100px] h-[100px]"
-        ></div>
+    <div className="h-[100vh]">
+      <div className="w-[100vw] h-[100vh] z-[50] flex items-center justify-center fixed top-0 right-0">
+        <div className="w-full flex items-center justify-center">
+          <div
+            style={{
+              backgroundImage: `url('${WiFiLoader}')`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center center",
+            }}
+            className="w-[100px] h-[100px]"
+          ></div>
+        </div>
       </div>
     </div>
   );
