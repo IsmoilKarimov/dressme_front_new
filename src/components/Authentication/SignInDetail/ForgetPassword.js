c830faa528ad59fe152a9e0e72f9cffd8ecb4660
Sardor0795 <english.programmer.9595@gmail.com>
2024-02-01 12:43:19 +0500

    data

--- src/components/Authentication/SignInDetail/ForgetPassword.js
+++ src/components/Authentication/SignInDetail/ForgetPassword.js
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { PhoneIcons, SircleNext } from "../../../assets/icons";
+import { PhoneIcons, SircleNext, UserMailIcon } from "../../../assets/icons";
 
 export default function ForgetPassword() {
   return (
-    <div className=" py-8 w-full min-h-[calc(100vh-180px)] flex justify-center ss:px-4 md:px-0">
+    <div className="mt-[180px] py-8 w-full min-h-[calc(100vh-180px)] flex justify-center ss:px-4 md:px-0">
       <div className="max-w-[440px] w-[100%] h-fit  md:px-[40px] md:py-[32px] ss:p-5 border border-searchBgColor rounded-lg">
         <div className=" w-full mt-1 mb-7 flex flex-col justify-center">
           <span className="not-italic font-AeonikProMedium text-xl ss:text-start md:text-center leading-5   tracking-[0,16px] text-black">
@@ -16,18 +16,20 @@ export default function ForgetPassword() {
 
         <div className="mt-1 w-full h-fit">
           <div className=" not-italic font-AeonikProRegular text-sm leading-4 text-black  tracking-[0.16px]">
-            Номер телефона / Электронная почта
+            Электронная почта
           </div>
-          <div className="mt-[6px] px-[16px] w-full flex items-center bg-btnBgColor border border-searchBgColor rounded-lg ">
+          <div className="mt-1 xs:mt-[6px]  w-full flex items-center bg-btnBgColor border border-searchBgColor rounded-lg ">
             <input
-              className="  w-full h-12 bg-btnBgColor focus:bg-btnBgColor active:bg-btnBgColor placeholder:bg-btnBgColor placeholder-not-italic placeholder-font-AeonikProMedium placeholder-text-base placeholder-leading-4 placeholder-text-black "
-              type="text"
-              name="name"
-              placeholder="Phone number"
+              className="w-full h-[42px] pl-2 xs:pl-[16px] bg-btnBgColor focus:bg-btnBgColor active:bg-btnBgColor placeholder:bg-btnBgColor placeholder-not-italic placeholder-font-AeonikProMedium placeholder-text-base placeholder-leading-4 placeholder-text-black "
+              type="email"
+              name="email"
+              // value={state?.email}
+              // onChange={(e) => setState({ ...state, email: e.target.value })}
+              placeholder="example@mail.com"
               required
             />
-            <span>
-              <PhoneIcons colors={"#000"} />
+            <span className="pr-2 xs:pr-[16px]">
+              <UserMailIcon />
             </span>{" "}
           </div>
         </div>
@@ -37,7 +39,7 @@ export default function ForgetPassword() {
           className="mt-8  border cursor-pointer flex items-center justify-center border-searchBgColor w-full h-12 bg-SignInBgColor select-none rounded-lg active:scale-95	active:opacity-70 "
         >
           <span className="not-italic font-AeonikProMedium mr-2 text-base leading-4 text-center text-white tracking-[0,16px]">
-            Сбросит пароль{" "}
+            Сбросить пароль{" "}
           </span>
           <span>
             <SircleNext colors={"#fff"} />
