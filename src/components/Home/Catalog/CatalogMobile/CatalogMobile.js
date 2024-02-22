c84e1dd9dee28665989d8a035a14e96667822022
main <ismoilergashovich@gmail.com>
2024-02-22 15:37:55 +0500

    new changes for catalog mobile

--- src/components/Home/Catalog/CatalogMobile/CatalogMobile.js
+++ src/components/Home/Catalog/CatalogMobile/CatalogMobile.js
@@ -1,14 +1,5 @@
 import React, { useContext, useState } from "react";
-import {
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-} from "../../../../assets";
+import { img4, img5, img6, img7, img8 } from "../../../../assets";
 import { MenuCloseIcons } from "../../../../assets/icons";
 import { dressMainData } from "../../../../ContextHook/ContextMenu";
 import { useNavigate } from "react-router-dom";
@@ -18,11 +9,11 @@ const CatalogMobile = () => {
 
   const [openCatalog, setOpenCatalog] = useState(false);
   const wearArray = [
-    { id: 4, name: "Головные уборы", img: img4 },
-    { id: 5, name: "Верхняя одежда", img: img5 },
-    { id: 6, name: "Нижняя одежда", img: img6 },
-    { id: 7, name: "Обувь", img: img7 },
-    { id: 8, name: "Аксессуары", img: img8 },
+    { id: 1, name: "Головные уборы", img: img4 },
+    { id: 2, name: "Верхняя одежда", img: img5 },
+    { id: 3, name: "Нижняя одежда", img: img6 },
+    { id: 4, name: "Обувь", img: img7 },
+    { id: 5, name: "Украшения / Аксессуары", img: img8 },
   ];
   const listItem = [
     { id: 1, name: "Футболки и майки" },
@@ -40,27 +31,31 @@ const CatalogMobile = () => {
     { id: 13, name: "Пиджаки и жилеты" },
     { id: 14, name: "Религиозная одежда для мужчин" },
   ];
-  // /catalog/:id
 
   const navigate = useNavigate();
   const goCatalogId = (id) => {
-    navigate(`/catalog/:${id}`);
+    navigate(`/catalog/${id}`);
   };
+
   return (
     <main className="flex flex-col justify-center items-center m-0 p-0 box-border">
       <section className="max-w-[1280px] w-[100%] ss:px-4 md:px-0 flex justify-center items-center m-auto border-t md:border-0 border-searchBgColor">
-        <article className="w-full h-fit my-8 flex flex-wrap  ll:gap-x-2 gap-y-4 justify-between">
-          {wearArray?.map((data) => {
+        <article className="w-full h-full pt-6 pb-20 flex flex-wrap ll:gap-x-2 gap-y-4 justify-between">
+          {wearArray?.map((item) => {
             return (
-              <figure className="w-[140px] ls:w-[150px] ll:w-[175px] h-fit flex flex-wrap gap-y-2 ">
-                <div className="w-full h-[145px] ls:h-[155px] ll:h-[180px] flex items-center overflow-hidden justify-center border border-skeltonColor bg-categoryModalBgColor rounded-[12px]">
-                  <img src={data?.img} alt="" className=" h-full	" />
+              <figure key={item?.id} className="w-[140px] ls:w-[150px] ll:w-[175px] h-fit flex flex-wrap gap-y-2 ">
+                <div
+                  onClick={() => goCatalogId(item?.id)}
+                  className="w-full h-[145px] ls:h-[155px] ll:h-[180px] flex items-center overflow-hidden justify-center border border-skeltonColor bg-categoryModalBgColor rounded-[12px]"
+                >
+                  <img src={item?.img} alt="" className=" h-full	" />
                 </div>
                 <button
-                  onClick={() => setOpenCatalog(true)}
-                  className="w-full h-8 text-catalogText leading-normal text-[13px] not-italic font-AeonikProRegular flex items-center justify-center active:scale-95  active:opacity-70 border border-skeltonColor  rounded-[12px]"
+                  //   onClick={() => setOpenCatalog(true)}
+                  onClick={() => goCatalogId(item?.id)}
+                  className="w-full h-8 text-catalogText leading-normal text-[12px] ll:text-[13px] not-italic font-AeonikProRegular flex items-center justify-center active:scale-95  active:opacity-70 border border-skeltonColor  rounded-[12px]"
                 >
-                  {data?.name}
+                  {item?.name}
                 </button>
               </figure>
             );
@@ -70,15 +65,15 @@ const CatalogMobile = () => {
       <section
         className={`w-full md:hidden h-screen ${
           openCatalog
-            ? " flex flex-col ease-linear duration-500 overscroll-none"
+            ? "flex flex-col ease-linear duration-500 overscroll-none"
             : "left-[-500px] lg:left-[-1000px] ease-linear duration-500"
         }	bg-white fixed z-[110] top-0 left-0 `}
       >
-        <div className="w-full flex flex-col  p-4">
-          <div className="w-full flex  justify-end">
+        <div className="w-full flex flex-col p-4">
+          <div className="w-full flex justify-end">
             {" "}
             <button
-              className="w-10 h-10 rounded-lg bg-white border border-searchBgColor flex items-center justify-center active:scale-95  active:opacity-70"
+              className="w-10 h-10 rounded-lg bg-white border border-searchBgColor flex items-center justify-center active:scale-95 active:opacity-70"
               onClick={() => setOpenCatalog(false)}
             >
               {" "}
@@ -88,10 +83,12 @@ const CatalogMobile = () => {
           <div className="bg-white mt-5">
             <ul>
               {listItem.map((e) => {
+                console.log();
                 return (
                   <li
+                  key={e?.id}
                     onClick={() => {
-                      goCatalogId(e?.name);
+                      goCatalogId(e?.id);
                       setOpenCatalog(false);
                     }}
                     className={`text-catalogList text-lg mt-3 leading-normal not-italic font-AeonikProRegular ${dressInfo?.TextHoverSeason}`}
