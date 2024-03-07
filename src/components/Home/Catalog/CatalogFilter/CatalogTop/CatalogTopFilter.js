be2170ab05571f4f7112f971ae864079091a32f9
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2024-03-06 23:25:46 -1100

    Single page update
    Single page update

--- src/components/Home/Catalog/CatalogFilter/CatalogTop/CatalogTopFilter.js
+++ src/components/Home/Catalog/CatalogFilter/CatalogTop/CatalogTopFilter.js
@@ -11,8 +11,8 @@ const CatalogTopFilter = ({ paramId }) => {
     <main className="w-full">
       <section className="w-full h-full   ">
         <div className="flex flex-col border-b border-searchBgColor py-3 md:pt-8 md:pb-5 w-full justify-center items-center ">
-          <section className="max-w-[1280px] w-[100%] h-full flex items-center justify-between m-auto">
-            <nav className="w-[100%] md:w-fit flex items-center py-1 md:p-0">
+          <section className="max-w-[1280px] w-[100%] h-full flex items-center justify-between m-auto px-4 md:px-0">
+            <nav className="w-[100%] md:w-fit flex items-center py-1 md:p-0  ">
               <NewBreadCrump items={breadcrumbItems} />
             </nav>
           </section>
