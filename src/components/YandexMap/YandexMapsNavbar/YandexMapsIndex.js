b413af82d3fdcde52d9f5ed4355f0cac32e5a6eb
Murod <74758823+MurodjohnKhayitov@users.noreply.github.com>
2024-03-07 20:20:56 -1100

    data
    aata

--- src/components/YandexMap/YandexMapsNavbar/YandexMapsIndex.js
+++ src/components/YandexMap/YandexMapsNavbar/YandexMapsIndex.js
@@ -25,18 +25,23 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
   const toggleRegionsHide = useCallback(() => setRegionsShow(false), []);
   const navigate = useNavigate();
 
-  const goCatalogId = (id, name) => {
-    if (id !== 5) {
-      navigate(`/categories/${name?.split(" ")?.join("-")?.toLowerCase()}`);
+
+  const goCatalogId = (id, nameru, nameuz) => {
+    if (languageDetector?.typeLang === 'ru') {
+      if (id !== 5) {
+        navigate(`/categories/${nameru?.split(' ')?.join('-')?.toLowerCase()}`);
+      }
+      if (id === 5) {
+        navigate(`/categories/${nameru?.split('/')?.map(item => item.trim())?.join('-')?.toLowerCase()}`);
+      }
     }
-    if (id === 5) {
-      navigate(
-        `/categories/${name
-          ?.split("/")
-          ?.map((item) => item.trim())
-          ?.join("-")
-          ?.toLowerCase()}`
-      );
+    if (languageDetector?.typeLang === 'uz') {
+      if (id !== 5) {
+        navigate(`/categories/${nameuz?.split(' ')?.join('-')?.toLowerCase()}`);
+      }
+      if (id === 5) {
+        navigate(`/categories/${nameuz?.split('/')?.map(item => item.trim())?.join('-')?.toLowerCase()}`);
+      }
     }
   };
   return (
@@ -48,9 +53,8 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
         ></div>
       )}
       <article
-        className={`fixed top-[235px] z-[251] left-[52.9%] right-1/2 overflow-hidden translate-x-[-50%] translate-y-[-50%] inset-0 w-fit h-fit shadow-modalCategoryShadow transform tras ${
-          dressInfo?.openCatologId ? "" : "hidden"
-        }`}
+        className={`fixed top-[235px] z-[251] left-[52.9%] right-1/2 overflow-hidden translate-x-[-50%] translate-y-[-50%] inset-0 w-fit h-fit shadow-modalCategoryShadow transform tras ${dressInfo?.openCatologId ? "" : "hidden"
+          }`}
       >
         <div className="flex justify-center items-center z-[99999]">
           <div className="w-[675px] flex flex-col shadow-modalCategoryShadow bg-white rounded-lg p-2">
@@ -74,7 +78,7 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
                       className="w-1/5 flex items-center justify-center "
                     >
                       <figure
-                        onClick={() => goCatalogId(data?.id, data?.name_ru)}
+                        onClick={() => goCatalogId(data?.id, languageDetector?.typeLang === 'ru' && data?.name_ru, languageDetector?.typeLang === 'uz' && data?.name_uz)}
                         className="group cursor-pointer"
                       >
                         <div className="group-hover:border-black transition duration-300 w-[120px] h-[120px] border border-categoryModalBorderColor bg-categoryModalBgColor flex items-center justify-center rounded-xl">
@@ -101,11 +105,10 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
       {regionsShow && (
         <div
           className={`max-w-[600px]  w-full fixed duration-500 z-[231]  left-1/2 right-1/2 top-[50%] translate-x-[-50%] translate-y-[-50%]  h-fit flex items-center  justify-center mx-auto
-        ${
-          regionsShow
-            ? " bottom-0 md:flex flex-col"
-            : "bottom-[-1500px] z-[-10]"
-        }
+        ${regionsShow
+              ? " bottom-0 md:flex flex-col"
+              : "bottom-[-1500px] z-[-10]"
+            }
         `}
         >
           <RegionListYandex onClick={toggleRegionsHide} />
@@ -114,11 +117,10 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
       <div className="max-w-[1320px] w-[100%] ss:block md:flex  md:py-0 justify-center items-center m-auto">
         <div className="">
           <div
-            className={`ss:px-3 mt-[1px] md:mt-0 md:px-[40px] md:rounded-b-[16px] ${
-              !dressInfo?.yandexOpenMarket
+            className={`ss:px-3 mt-[1px] md:mt-0 md:px-[40px] md:rounded-b-[16px] ${!dressInfo?.yandexOpenMarket
                 ? "bg-yandexNavbar backdrop-blur-md"
                 : "bg-white"
-            }	border border-searchBgColor border-t-0`}
+              }	border border-searchBgColor border-t-0`}
           >
             <div className="hidden md:block">
               {" "}
@@ -132,11 +134,10 @@ function YandexMapsIndex({ getMapsInfo, getFilterSearchByBrand }) {
           </div>
           {!dressInfo?.yandexOpenMarket && (
             <div
-              className={`flex flex-col justify-center  fixed left-0 right-0 z-55 ss:top-auto ${
-                !dressInfo?.yandexFullScreen
+              className={`flex flex-col justify-center  fixed left-0 right-0 z-55 ss:top-auto ${!dressInfo?.yandexFullScreen
                   ? "ss:bottom-[63px] "
                   : "ss:bottom-[0]"
-              }
+                }
 
                md:bottom-auto
               duration-500
