bf4d5cd80f5b9c96ccae64af1f18265c0b4e38c0
Sardor0795 <english.programmer.9595@gmail.com>
2024-03-12 19:22:30 +0500

    bug fix

--- src/components/Category/CategoryForType/CategoryElement/CategoryCards.js
+++ src/components/Category/CategoryForType/CategoryElement/CategoryCards.js
@@ -51,8 +51,8 @@ export default function CategoryCards({ filterData, setPageId, paramsId }) {
       </section>
 
       <section className="w-full  h-fit flex items-center justify-center mt-[75px] gap-x-6">
-        <article className="flex  items-center md:justify-center">
-          <ul className="flex items-center md:justify-center overflow-x-auto pb-[8px] md:pb-0">
+        <article className="flex w-full mx-auto items-center overflow-x-auto md:justify-center">
+          <ul className="flex w-fit mx-auto items-center md:justify-center  pb-[8px] md:pb-0">
             {filterData?.section_products?.links?.map((item, index) => {
               return (
                 <li
--- src/components/Home/Shop/ShoppingStoreOfficial/ShoppingStoreCategory/ShopOfficialCards/ShopOfficialCard.js
+++ src/components/Home/Shop/ShoppingStoreOfficial/ShoppingStoreCategory/ShopOfficialCards/ShopOfficialCard.js
@@ -210,8 +210,8 @@ export default function ShopOfficialCard({
       </div>
 
       <section className="w-full  h-fit flex items-center justify-center mt-[75px] gap-x-6">
-        <article className="flex items-center md:justify-center">
-          <ul className="flex items-center md:justify-center overflow-x-auto pb-[8px] md:pb-0">
+        <article className="flex w-full mx-auto items-center md:justify-center overflow-x-auto">
+          <ul className="flex w-fit mx-auto items-center md:justify-center  pb-[8px] md:pb-0">
             {filteredData?.products?.links?.map((item, index) => {
               return (
                 <li
