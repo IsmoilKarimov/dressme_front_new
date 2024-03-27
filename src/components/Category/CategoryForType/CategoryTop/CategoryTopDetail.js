bb9029ec514f47873223c179151cf33049731316
Sardor0795 <english.programmer.9595@gmail.com>
2024-03-27 10:33:23 +0500

    bug fix

--- src/components/Category/CategoryForType/CategoryTop/CategoryTopDetail.js
+++ src/components/Category/CategoryForType/CategoryTop/CategoryTopDetail.js
@@ -146,15 +146,15 @@ const CategoryTopDetail = ({
             <article className="w-full flex flex-col border-b md:border-none border-searchBgColor px-4 md:px-0">
               <figure className="relative w-full md:h-[90px] my-10 md:mt-0 h-fit flex flex-row items-center justify-between border border-searchBgColor rounded-lg">
                 {/*  */}
-                <div className="w-fit flex h-[66px] md:h-fit items-center   rounded-lg  ">
-                  <div className="absolute w-[80px] h-[120px] md:w-[120px] md:h-[160px] overflow-hidden left-[38px] md:left-[40px] rounded-xl border border-searchBgColor flex items-center justify-center bg-white columns-1">
+                <div className="w-fit flex h-[66px] md:h-fit items-center   rounded-lg">
+                  <div className="absolute w-[80px] h-[120px] md:w-[120px] md:h-[160px] overflow-hidden left-[16px] md:left-[40px] rounded-xl border border-searchBgColor flex items-center justify-center bg-white columns-1">
                     <img
                       src={filterData?.section?.url_photo}
                       alt=""
                       className="w-full h-full rounded-xl object-cover object-top"
                     />
                   </div>
-                  <div className="flex items-center ml-[130px] md:ml-[210px]">
+                  <div className="flex items-center ml-[106px] md:ml-[210px]">
                     <div className="text-xl font-AeonikProMedium">
                       {languageDetector?.typeLang === "ru" &&
                         filterData?.section?.name_ru}
